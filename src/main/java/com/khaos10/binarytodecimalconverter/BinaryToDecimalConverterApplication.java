6df91602dfd036f24d75ac72b3029362e109e086
Kaho <nagata.kaho10@gmail.com>
2022-08-05 09:33:41 +0200

    CREATE: Spring Boot Project with Gradle

---
+++ src/main/java/com/khaos10/binarytodecimalconverter/BinaryToDecimalConverterApplication.java
@@ -0,0 +1,13 @@
+package com.khaos10.binarytodecimalconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryToDecimalConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinaryToDecimalConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khaos10/binarytodecimalconverter/BinaryToDecimalConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khaos10.binarytodecimalconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaryToDecimalConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
