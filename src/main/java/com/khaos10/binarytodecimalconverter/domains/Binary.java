3f876a02942c79a6bf069f4beafd52e2dcf30f3d
Kaho <nagata.kaho10@gmail.com>
2022-08-09 09:56:21 +0200

    CREATE: Binary, Decimal domains

---
+++ src/main/java/com/khaos10/binarytodecimalconverter/domains/Binary.java
@@ -0,0 +1,19 @@
+package com.khaos10.binarytodecimalconverter.domains;
+
+public class Binary {
+
+    private String binary;
+
+    public Binary(String binary) {
+        this.binary = binary;
+    }
+    
+    public String getBinary() {
+        return binary;
+    }
+
+    public void setBinary(String binary) {
+        this.binary = binary;
+    }
+
+}
---
+++ src/main/java/com/khaos10/binarytodecimalconverter/domains/Decimal.java
@@ -0,0 +1,18 @@
+package com.khaos10.binarytodecimalconverter.domains;
+
+public class Decimal {
+
+    private int decimal;
+
+    public Decimal(int decimal) {
+        this.decimal = decimal;
+    }
+
+    public int getDecimal() {
+        return decimal;
+    }
+
+    public void setDecimal(int decimal) {
+        this.decimal = decimal;
+    }
+}
